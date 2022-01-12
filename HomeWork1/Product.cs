8cb81cc768127110910d69c417aae076504f5bee
MeGaComputers <MeGaComputers@DESKTOP-NREEBML>
2022-01-12 13:27:22 +0400

    Add project files.

---
+++ HomeWork1/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    public class Product
+    {
+        public int ID;
+        public string Name;
+        public double Price;
+
+        public Product(int id,string name,double price)
+        {
+            ID = id;
+            Name = name;
+            Price = price;
+        }
+
+        public void ChangeName()
+        {
+            Name = "SEO";
+        }
+
+        public void PercentCalculator()
+        {
+            Console.WriteLine(Price*0.18);
+        }
+    }
+}
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using HomeWork1;
+
+Product Product1 = new Product(1,"unnamed",1000);
+
+Product1.ChangeName();
+Console.WriteLine("Product ID: " + Product1.ID); 
+Console.WriteLine("Product Name: " + Product1.Name);   
+Console.WriteLine("Product Price: " + Product1.Price);
+Console.Write("18% of product price : ");
+Product1.PercentCalculator();
+
+
+
